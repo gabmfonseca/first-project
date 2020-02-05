6192c383b3a09844ae666f98ba9f93e4d1063683
Gabriela Fonseca <gabmfonseca@gmail.com>
2020-02-05 11:49:03 +0000

    changed images

--- scripts/images.js
+++ scripts/images.js
@@ -1,12 +1,12 @@
 const boneImage = new Image();
-boneImage.src = '../images/bone@2x.png';
+boneImage.src = '../images/bone.png';
 
 const meteorImage = new Image();
 // meteorImage.src = '../images/meteor.png';
 meteorImage.src = '../images/meteor_sprite.png';
 
 const robotImage = new Image();
-// robotImage.src = '../images/robotmoving.svg';
+// robotImage.src = '../images/robotmoving.png';
 robotImage.src = '../images/robot_sprite.png';
 
 const rockImage = new Image();
